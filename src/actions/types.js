85f0d1478bb5f6056978d22920bbed92f55ad07e
dreamer20 <gv4alex@gmail.com>
2019-01-26 00:32:35 +0300

    Update

--- src/actions/types.js
+++
@@ -1,7 +0,0 @@
-
-export const RECEIVE_TRACKS = 'RECEIVE_TRACKS';
-export const REMOVE_TRACKS = 'REMOVE_TRACKS';
-
-export const SELECT_TRACK = 'SELECT_TRACK';
-export const SET_ON_PAUSE = 'SET_ON_PAUSE';
-export const SET_ON_PLAY = 'SET_ON_PLAY';
